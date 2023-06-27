b310ff2ac86fc1636e1127f6d47700cbe145b580
Alex Patel <apatel@mymail.mines.edu>
2023-06-26 19:15:18 -0600

    Initial Commit

---
+++ Gym Buddy/Gym_BuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gym_BuddyApp.swift
+//  Gym Buddy
+//
+//  Created by Alex Patel on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct Gym_BuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
